b25d789e47000746ca85322a46c14a4c99181ad0
marnvel <ricardom.velarde@gmail.com>
2022-09-14 12:28:27 -0300

    Se crearon archivos home.routes.js home.controllers.js. Se creó la carpeta db. 14-09-2022 12:28hs

--- Construccion_de_servidor_Velarde/index.js
+++ Construccion_de_servidor_Velarde/index.js
@@ -1,11 +1,25 @@
 
 //*Importo librerías
-
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const home = require('./src/routes/home.routes');
 
 
-const app = express();
 
-const port = 3000;
+const app = express();//Inicializo la librería express
 
-app.listen(port, () => console.log(`Servidor corriendo en " http://localhost:${port} " `));
+
+//*Configuraciones
+const port = process.env.PORT || 3000;
+
+//*MIDDLEWARES
+app.use(cors());//PAra admitir peticiones de cualquier lado.
+app.use(morgan('dev'));
+app.use(express.json()); // Para que el servidor comprenda archivos con formato jso
+app.use(home);
+
+//*Inicializo el servidor!
+app.listen(port, () => console.log(`Servidor corriendo en " http://localhost:${port} ". Servidor iniciado en el puerto: ${port}`));
---
+++ Construccion_de_servidor_Velarde/src/routes/home.routes.js
@@ -0,0 +1,37 @@
+// El método Router() de Express nos permite crear rutas
+const router = require('express').Router();
+
+
+//*Importandoo controladores
+const {
+    getHome,
+    postHome,
+    putHome,
+    deleteHome,
+
+} = require('../controllers/home.controllers');
+
+
+
+
+//Request handler. Manejador de petición. el callback me permite definir el comportamiento de lo que tiene que hacer.
+//El callback me define el comportamiento del controlador.
+
+//*Definiendo rutas
+
+router.get('/', getHome);
+// router.get('/', (req,res)=>console.log(res.json("ok")));
+
+
+router.post('/', postHome);
+
+
+router.put('/', putHome);
+
+
+router.delete('/', deleteHome);
+
+
+
+// Se exporta el objeto router que contiene las rutas
+module.exports = router;
